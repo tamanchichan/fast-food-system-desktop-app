ea748de9f7dfebe52a9c3558adebf41c57dc9dec
tamanchichan <tamanchichan@gmail.com>
2025-07-14 13:11:23 -0500

    "Data" folder, and "DataAccess.cs" class created

---
+++ fast-food-system-desktop-app/Data/DataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fast_food_system_desktop_app.Data
+{
+    public class DataAccess
+    {
+
+    }
+}
