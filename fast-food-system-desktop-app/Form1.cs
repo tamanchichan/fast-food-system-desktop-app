2f2feacabec8e28dcfb5f60fe494294343f5fc42
tamanchichan <tamanchichan@gmail.com>
2025-07-14 13:09:58 -0500

    Add project files.

---
+++ fast-food-system-desktop-app/Form1.cs
@@ -0,0 +1,10 @@
+namespace fast_food_system_desktop_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ fast-food-system-desktop-app/Program.cs
@@ -0,0 +1,17 @@
+namespace fast_food_system_desktop_app
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
